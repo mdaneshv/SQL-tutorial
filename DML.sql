eb3ecff8f9381aadbe04bd384537d938dbf15d0c
mdaneshv <58681626+mdaneshv@users.noreply.github.com>
2020-11-27 17:39:56 -0600

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -1,11 +1,24 @@
 
--- Delete statement 
+use database classicmodel;
+
+-- Insert into a table from another table
+INSERT INTO dup_orders
+  SELECT * FROM classicmodels.orders;
+  
+  
+-- Insert manually
+INSERT INTO dup_orders (orderNumber, orderDate)
+  VALUES (100, '3000-01-01'),
+  (300 ,'9000-01-01');
+  
+
+-- Delete statement: it deletes existing records from the table based on the condition
 DELETE FROM dup_orders 
 WHERE
     orderNumber = 100;
 
 
--- Update tabel
+-- Update tablw
 UPDATE dup_orders 
 SET 
     orderDate = '9999-01-01'
