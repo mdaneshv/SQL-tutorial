d494a67d609e6c9ae342d12862b582aa03f3f0e9
mdaneshv <58681626+mdaneshv@users.noreply.github.com>
2020-11-27 17:00:23 -0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -82,14 +82,31 @@ ORDER BY summation;
 COMMIT; -- A transaction control language used to save the changes permanently.
         -- Should be used specially before delete, insert or update statements
 
-DELETE FROM dup_orders WHERE orderNumber=100;
-SELECT * FROM dup_orders ORDER BY orderNumber ASC;
+DELETE FROM dup_orders 
+WHERE
+    orderNumber = 100;
+SELECT 
+    *
+FROM
+    dup_orders
+ORDER BY orderNumber ASC;
+
+
 -- Rollback changes it back to the last commit.
-ROLLBACK; 
-SELECT * FROM dup_orders;
+ROLLBACK;
+SELECT 
+    *
+FROM
+    dup_orders;
 
 
 
-COMMIT; -- Changes are saved and this transaction can not be undone.
-UPDATE dup_orders SET orderDate='9999-01-01' WHERE orderNumber=10100;
-ROLLBACK; -- Changes back to the last commit.
+COMMIT;-- Changes are saved and this transaction can not be undone.
+
+UPDATE dup_orders 
+SET 
+    orderDate = '9999-01-01'
+WHERE
+    orderNumber = 10100;
+    
+ROLLBACK; --  change it back to the last commit.
