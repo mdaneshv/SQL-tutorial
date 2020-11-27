b338149803049065cb0fc0d3d10a784baf41f66f
mdaneshv <58681626+mdaneshv@users.noreply.github.com>
2020-11-27 17:42:26 -0600

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -11,6 +11,12 @@ INSERT INTO dup_orders (orderNumber, orderDate)
   VALUES (100, '3000-01-01'),
   (300 ,'9000-01-01');
   
+  
+-- Insert into the table from a csv or txt file. Use FIRSTROW=2 to skip the column headers.
+LOAD DATA INFILE 'C:\customers.csv'
+INTO TABLE dup_orders
+FIELDS TERMINATED BY ',';  
+  
 
 -- Delete statement: it deletes existing records from the table based on the condition
 DELETE FROM dup_orders 
