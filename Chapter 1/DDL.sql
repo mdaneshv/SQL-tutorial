afcb812d44925f1dd22224953a377d00daafe79d
Mohammad Daneshvar <58681626+mdaneshv@users.noreply.github.com>
2020-11-27 22:42:54 -0600

    Update DDL.sql

--- Chapter 1/DDL.sql
+++ Chapter 1/DDL.sql
@@ -89,7 +89,7 @@ insert  into `employees`(`employeeNumber`,`lastName`,`firstName`,`extension`,`em
 
 (1088,'Grant','Alan','x4871','grantal@company.com','6',1056,'Sales Manager (APAC)'),
 
-(1102,'Malcolm','Ian','x5408','malian@company.com','4',1056,'Sale Manager (EMEA)'),
+(1102,'Malcolm','Ian','x5408','malian@company.com','4',1056,'Sale Manager (EMEA)');
 
 
 DROP TABLE IF EXISTS `offices`;
@@ -99,7 +99,6 @@ CREATE TABLE `offices` (
   `city` varchar(50) NOT NULL,
   `phone` varchar(50) NOT NULL,
   `addressLine1` varchar(50) NOT NULL,
-  `addressLine2` varchar(50) DEFAULT NULL,
   `state` varchar(50) DEFAULT NULL,
   `country` varchar(50) NOT NULL,
   `postalCode` varchar(15) NOT NULL,
@@ -107,19 +106,14 @@ CREATE TABLE `offices` (
   PRIMARY KEY (`officeCode`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-insert  into `offices`(`officeCode`,`city`,`phone`,`addressLine1`,`addressLine2`,`state`,`country`,`postalCode`,`territory`) values 
+insert  into `offices`(`officeCode`,`city`,`phone`,`addressLine1`,`state`,`country`,`postalCode`,`territory`) values 
 
-('1','San Francisco','+1 650 219 4782','100 Market Street','Suite 300','CA','USA','94080','NA'),
+('1','San Francisco','+1 123 123 3245','100 Market Street','CA','USA','94080','NA'),
 
-('2','Boston','+1 215 837 0825','1550 Court Place','Suite 102','MA','USA','02107','NA'),
+('2','Boston','+1 222 222 2222','1550 Court Place','MA','USA','02107','NA'),
 
-('3','NYC','+1 212 555 3000','523 East 53rd Street','apt. 5A','NY','USA','10022','NA'),
+('3','NYC','+1 333 333 3333','523 East 53rd Street','NY','USA','10022','NA'),
 
-('4','Paris','+33 14 723 4404','43 Rue Jouffroy D\'abbans',NULL,NULL,'France','75017','EMEA'),
+('4','Paris','+444 444 4444','43 Rue Jouffroy D\'abbans',NULL,'France','75017','EMEA'),
 
-('5','Tokyo','+81 33 224 5000','4-1 Kioicho',NULL,'Chiyoda-Ku','Japan','102-8578','Japan'),
-
-('6','Sydney','+61 2 9264 2451','5-11 Wentworth Avenue','Floor #2',NULL,'Australia','NSW 2010','APAC'),
-
-('7','London','+44 20 7877 2041','25 Old Broad Street','Level 7',NULL,'UK','EC2N 1HN','EMEA');
-
+('5','Tokyo','+555 555 5555','4-1 Kioicho','Chiyoda-Ku','Japan','102-8578','Japan'),
